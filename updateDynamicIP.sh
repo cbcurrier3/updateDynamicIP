688d1423074daa3b158bee411d19833e6312bdc7
cbcurrier3 <32932945+cbcurrier3@users.noreply.github.com>
2019-05-03 10:31:29 -0400

    Update updateDynamicIP.sh
    Updated to R80.20, recurse all interfaces and update all along with maintaining topology and other interface settings of the gateway.

--- updateDynamicIP.sh
+++ updateDynamicIP.sh
@@ -1,8 +1,11 @@
 #/bin/bash
+# AUTHOR: Luca Fama - Initial work - https://github.com/lfama
+# Version R80.20 v1
+# Updated 5/3/19 by CB Currier
 #
-# A very simple bash script that uses Check Point R80.10 APIs in order to check and update your dynamic public IP address.
+# A very simple bash script that uses Check Point R80.20 APIs in order to check and update your dynamic public IP address.
 # https://github.com/lfama/updateDynamicIP
-# 
+#
 #
 ### Edit the following variables according to your needs
 # The Gateway object's name
@@ -17,14 +20,12 @@ POLICY="MyPolicy"
 USER="admin"
 # Administrator password
 PASSWORD="sup3rS3cr3t!"
-
-
 echo "$(date)"
 
 # You need this in order to run the script as a cron job
-export LD_LIBRARY_PATH=/opt/CPsuite-R80/fw1/oracle_oi/sdk:/opt/uf/SecureComputing/lib:/opt/KAV/ppl:/opt/KAV/lib:/opt/CPsuite-R80/fw1/oracle_oi/sdk:/opt/uf/SecureComputing/lib:/opt/KAV/ppl:/opt/KAV/lib:/opt/CPshrd-R80/lib:/opt/CPshrd-R80/web/Apache/lib:/opt/CPshrd-R80/database/postgresql/lib:/opt/CPshrd-R80/lib64:/opt/CPsuite-R80/fw1/lib:/opt/CPsuite-R80/fg1/lib:/opt/CPppak-R80/lib:/opt/CPdiag-R80/lib:/opt/CPportal-R80/lib:/opt/CPrt-R80/lib:/opt/CPrt-R80/log_indexer/lib:/opt/CPuepm-R80/lib:/opt/CPuepm-R80/apache22/lib:/opt/CPvsec-R80/lib:/opt/CPcvpn-R80/lib:/opt/CPshrd-R80/database/postgresql/lib:/opt/CPshrd-R80/lib64
+export LD_LIBRARY_PATH=/opt/CPsuite-R80.20/fw1/oracle_oi/sdk:/opt/uf/SecureComputing/lib:/opt/KAV/ppl:/opt/KAV/lib:/opt/CPsuite-R80.20/fw1/oracle_oi/sdk:/opt/uf/SecureComputing/lib:/opt/KAV/ppl:/opt/KAV/lib:/opt/CPshrd-R80.20/lib:/opt/CPshrd-R80.20/web/Apache/lib:/opt/CPshrd-R80.20/database/postgresql/lib:/opt/CPshrd-R80.20/lib64:/opt/CPsuite-R80.20/fw1/lib:/opt/CPsuite-R80.20/fg1/lib:/opt/CPppak-R80.20/lib:/opt/CPdiag-R80.20/lib:/opt/CPportal-R80.20/lib:/opt/CPrt-R80.20/lib:/opt/CPrt-R80.20/log_indexer/lib:/opt/CPuepm-R80.20/lib:/opt/CPuepm-R80.20/apache22/lib:/opt/CPvsec-R80.20/lib:/opt/CPcvpn-R80.20/lib:/opt/CPshrd-R80.20/database/postgresql/lib:/opt/CPshrd-R80.20/lib64
 
-OLD_IP="$(/opt/CPshrd-R80/bin/mgmt_cli show host name $PPP_OBJ -u $USER -p $PASSWORD | grep 'ipv4' | grep -oE '\b([0-9]{1,3}\.){3}[0-9]{1,3}\b')"
+OLD_IP="$(/opt/CPshrd-R80.20/bin/mgmt_cli --port 4434 show host name $PPP_OBJ -u $USER -p $PASSWORD | grep 'ipv4' | grep -oE '\b([0-9]{1,3}\.){3}[0-9]{1,3}\b')"
 
 PUBLIC_IP="$(/bin/cp-ifconfig.sh $PPP_IFACE | grep "inet addr" |  grep -oE '\b([0-9]{1,3}\.){3}[0-9]{1,3}\b' | head -1)"
 
@@ -51,19 +52,37 @@ else
   echo "IP changed.. Updating object, topology and installing policies.."
   echo ""
   echo "Updating PPP object IP address"
-  /opt/CPshrd-R80/bin/mgmt_cli set host name $PPP_OBJ ipv4-address $PUBLIC_IP -u $USER -p $PASSWORD ignore-warnings true
+  /opt/CPshrd-R80.20/bin/mgmt_cli --port 4434 set host name $PPP_OBJ ipv4-address $PUBLIC_IP -u $USER -p $PASSWORD ignore-warnings true
 
   echo "Updating gw object topology.."
-  GW_OBJ="$(/opt/CPshrd-R80/bin/mgmt_cli show simple-gateway name $GW_OBJ -u $USER -p $PASSWORD -f json)"
-  x=$(/opt/CPshrd-R80/bin/jq -r '.interfaces[]' <<< "${GW_OBJ}")
-  echo $x > tmp.txt
-  sed -i "s/$OLD_IP/$PUBLIC_IP/g" tmp.txt
-  sed -i "s/} {/}, {/g" tmp.txt
-  payload=[`cat tmp.txt`]
-  /opt/CPshrd-R80/bin/mgmt_cli set simple-gateway name $GW_OBJ ipv4-address "$PUBLIC_IP" interfaces "$payload" -u $USER -p $PASSWORD ignore-warnings true
-  
+
+  $(/opt/CPshrd-R80.20/bin/mgmt_cli --port 4434 show simple-gateway name $GW_OBJ -u $USER -p $PASSWORD -f json > /var/log/tmp/tmp.txt )
+  echo -n "" > /var/log/tmp/itmp.txt
+
+  ifaces=$(cat /var/log/tmp/tmp.txt |jq -s -r '.[]|.interfaces[]' | jq -s -r 'map([]) |length');
+  for ((a=0;a<$ifaces;a+=1));
+   do b=$(($a + 1));
+        resa=$( cat /var/log/tmp/tmp.txt |jq -s -r ".[] | .interfaces[$a] | with_entries(if .key == \"anti-spoofing-settings\" or .key == \"topology-settings\" or .key == \"security-zone-settings\" then empty else . end)| with_entries( .key |= \"interfaces.$b.\" + .)" );
+        echo -n $resa >> /var/log/tmp/itmp.txt;
+
+        resb=$( cat /var/log/tmp/tmp.txt |jq -s -r ".[] | .interfaces[$a] | with_entries(if .key == \"anti-spoofing-settings\" then . else empty end)| .[\"anti-spoofing-settings\"] |with_entries( .key |= \"interfaces.$b.anti-spoofing-settings.\" + .)" );
+        echo -n $resb >> /var/log/tmp/itmp.txt;
+
+        resc=$( cat /var/log/tmp/tmp.txt |jq -s -r ".[] | .interfaces[$a] | with_entries(if .key == \"topology-settings\" then . else empty end)| .[\"topology-settings\"] |with_entries( .key |= \"interfaces.$b.topology-settings.\" + .)" );
+        echo -n $resc >> /var/log/tmp/itmp.txt;
+
+        resd=$( cat /var/log/tmp/tmp.txt |jq -s -r ".[] | .interfaces[$a] | with_entries(if .key == \"security-zone-settings\" then . else empty end)| .[\"security-zone-settings\"] |with_entries( .key |= \"interfaces.$b.security-zone-settings.\" + .)" );
+        echo -n $resd >> /var/log/tmp/itmp.txt;
+   done
+
+  sed -i "s/$OLD_IP/$PUBLIC_IP/g" /var/log/tmp/itmp.txt
+  payload=$(cat /var/log/tmp/itmp.txt|sed -e "s/, \"/ /g" |sed -e "s/\":/ /g"|sed -e "s/\",/ /g"|sed -e "s/ }{ / /g"|sed -e "s/ }null{ / /g"|sed -e "s/{//g"|sed -e "s/}//g"|sed -e "s/\"interfaces/interfaces/g")
+
   echo "Installing policies"
-  /opt/CPshrd-R80/bin/mgmt_cli install-policy policy-package $POLICY access true threat-prevention false -u $USER -p $PASSWORD
+  /opt/CPshrd-R80.20/bin/mgmt_cli --port 4434 set simple-gateway name $GW_OBJ $payload -u $USER -p $PASSWORD ignore-warnings true  --format json
+  rm -rf /var/log/tmp/itmp.txt
+  rm -rf /var/log/tmp/tmp.txt
+
 fi
 
 echo ""
